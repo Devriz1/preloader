565849083abe1ad317d655fc1b8c161329d5da99
Mohammed Risal <94342524+Devriz1@users.noreply.github.com>
2025-05-13 02:07:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@
       const preloader = document.getElementById("preloader");
       preloader.classList.add("preloader-zoom-out");
       setTimeout(() => {
-        window.location.href = "main-website.html"; 
+        window.location.href = "https://globalvoiceacademy.netlify.app/"; 
       }, 1000);
     }, 5000);
